0935c96cb09cac4459ace9433ee2757a06bf199d
Warhammer <lmvg.1989@gmail.com>
2021-09-14 00:13:16 -0500

    definiendo funcion

--- Practicas/funciones.py
+++ Practicas/funciones.py
@@ -1,7 +1,9 @@
 #FUNCIONES
 #Una funcion es un bloque de codigo que incluye instrucciones para realizar una tarea especifica
 
-def saludar():
-    print("Hola Usuario")
 
-saludar()
+def saludar(nombre, edad):  #Definimos una funcion y sus parametros
+    print(f"Hola {nombre}, tu edad es: {edad}")
+
+
+saludar("Michel", 32) #Invocamos una funcion y enviamos los parametros
